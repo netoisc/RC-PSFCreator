fb156d8da7e8b7cc33a53f08601c40d8018e93ca
unknown <ramirezp.jorge@gmail.com>
2015-04-08 12:20:38 -0500

    Adding function validator class

--- js/services/validator.js
+++ js/services/validator.js
@@ -4,9 +4,55 @@ angular.module('PSFcreator')
     function Validator(type,value,message) {
         
         this.type = type || '';
-        this.value = value  ;
+        this.value = value || ''  ;
         this.message = message  ;
     }
+  
+  
+    //Validate the validatos :) 
+    //Example minLenght can´t have a value of  "/ab+c/" 
+    Validator.prototype.isValid = function(){
+      
+      //Validators permited : 
+      // -required
+      // -minLength
+      // -maxLength
+      // -fileExtension
+      //
+      
+      //value always be number  
+      if(this.type=="minLength"){
+        var isNumber = Number.isInteger(this.value);
+        if(isNumber&& this.value>0){
+          return true;
+        } else {
+          return false;
+        }
+      }
+        
+      //value always be number  
+      else if(this.type=="maxLength"){
+        var isNumber = Number.isInteger(this.value);
+        if(isNumber&& this.value>0){
+          return true;
+        } else {
+          return false;
+        }
+      }
+      else if(this.type =="regEx"){
+        if(this.value.length > 0){
+          return true
+        }
+        return false;
+      }
+      else if ( this.type == "required" ||  this.type == "fileExtension" ) {
+        return true;
+      } else {
+        //Any other value is not a valid validator
+        return false;
+      }
+    
+    };
   
     return Validator;
 });
